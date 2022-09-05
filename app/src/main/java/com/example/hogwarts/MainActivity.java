7569082e0c9e85f7e8e16346d2f5254f01f6f94b
Suraj Kumar <suraj.kumar@rsllearning.com>
2022-09-05 13:11:33 +0530

    ...

--- app/src/main/java/com/example/hogwarts/MainActivity.java
+++ app/src/main/java/com/example/hogwarts/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,MainActivity_type2.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
@@ -21,4 +30,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent=new Intent(this,Topper.class);
         startActivity(intent);
     }
+
 }
--- app/src/main/java/com/example/hogwarts/MainActivity_type2.java
+++ app/src/main/java/com/example/hogwarts/MainActivity_type2.java
@@ -2,7 +2,10 @@ package com.example.hogwarts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity_type2 extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity_type2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_type2);
+        Button button=findViewById(R.id.button31);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity_type2.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
